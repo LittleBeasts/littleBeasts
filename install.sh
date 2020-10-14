898dbae884810ee6871196c3113e8b7d926f3644
leon <leon@pop-os.localdomain>
2020-10-14 21:07:40 +0200

    updated venv, updated python

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "installing pygame and python dependencys"
+apt install python3-pip
+apt-get install python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev libfreetype6-dev
+apt-get install python3-venv
+
