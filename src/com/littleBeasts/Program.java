4c37940db9dfdb36306932ef3561e2ca85bad1b7
BigKahuna7385 <danielburger2@gmx.de>
2020-10-20 22:22:42 +0200

    template

--- src/com/littleBeasts/Program.java
+++ src/com/littleBeasts/Program.java
@@ -1,2 +1,13 @@
-package com.littleBeasts;public class Program {
+package com.littleBeasts;
+
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.resources.Resource;
+import de.gurkenlabs.litiengine.resources.Resources;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Game.init(args);
+        Game.start();
+    }
 }
