6ee2169031279f6e2c116153128496134787633b
BigKahuna7385 <danielburger2@gmx.de>
2020-10-29 22:28:53 +0100

    Main Menu, Ingame Menu added

---
+++ src/com/littleBeasts/GameState.java
@@ -0,0 +1,8 @@
+package com.littleBeasts;
+
+public enum GameState {
+    MENU,
+    INGAME,
+    INGAME_MENU,
+    GAME_OVER
+}
--- src/com/littleBeasts/PlayerInput.java
+++ src/com/littleBeasts/PlayerInput.java
@@ -12,6 +12,14 @@ public final class PlayerInput {
 
     public static void init() {
         // make the game exit upon pressing ESCAPE (by default there is no such key binding and the window needs to be shutdown otherwise, e.g. ALT-F4 on Windows)
-        Input.keyboard().onKeyPressed(KeyEvent.VK_ESCAPE, e -> System.exit(0));
+        Input.keyboard().onKeyPressed(KeyEvent.VK_ESCAPE, e -> {
+            //System.exit(0);
+            if (GameLogic.getState() == GameState.INGAME) {
+                GameLogic.setState(GameState.INGAME_MENU);
+                System.out.println("Menu");
+            }else{
+                GameLogic.setState(GameState.INGAME);
+            }
+        });
     }
 }
