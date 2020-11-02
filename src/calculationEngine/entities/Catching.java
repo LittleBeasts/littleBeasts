acbe1e3c5c39ba17f33aedd213892a4082bf8c63
BigKahuna7385 <danielburger2@gmx.de>
2020-11-02 12:44:40 +0100

    fixed debug variable names and gitignore

--- src/calculationEngine/entities/Catching.java
+++ src/calculationEngine/entities/Catching.java
@@ -4,29 +4,29 @@ import java.util.Random;
 
 public class Catching {
     private static Random rnd = new Random();
-    private static String str; //string to store debug information
-    private static boolean debug = false; //show rolls in console
+    private static String debugInfo; //string to store debug information
+    private static boolean bDebug = false; //show rolls in console
 
     public static boolean isCaught(CeEntity player, CeEntity beast, Item cage) {
         //roll to d50 to get a quasi normal distribution
         int attackRoll = rnd.nextInt(51) + rnd.nextInt(51);
-        str = "Roll: " + attackRoll + "\n";
+        debugInfo = "Roll: " + attackRoll + "\n";
         Attack catchingSkill = player.getAttacks()[0]; //TODO: We need to specify, which skill the catching skill will be.
 
         if (catchingMisses(player, beast, catchingSkill, attackRoll, cage)) {
-            str += "Miss\n";
+            debugInfo += "Miss\n";
             return false;
         }
-        str += "Catch\n";
+        debugInfo += "Catch\n";
 
-        if (debug) System.out.println(str);
+        if (bDebug) System.out.println(debugInfo);
         return true;
     }
 
     private static boolean catchingMisses(CeEntity player, CeEntity beast, Attack attack, int attackRoll, Item cage) {
         // when it is a critical hit, it will never miss
         if (attackRoll > (100 - attack.getCriticalChance())) {
-            str += "Critical Catch\n";
+            debugInfo += "Critical Catch\n";
             return false;
         }
         // base difficulty is percent of the HP of the beast, then add the speed of the defender. After that the speed of the attacker and the accuracy of the attack are subtracted.
@@ -34,7 +34,7 @@ public class Catching {
         double levelModifier = player.getLevel() / beast.getLevel();
         int speed = (int) (player.getSpeed() * levelModifier);
         int difficulty = baseDifficultly + beast.getSpeed() - (speed + attack.getAccuracy() + cage.getBonus());
-        str += "Difficulty: " + difficulty + "\n";
+        debugInfo += "Difficulty: " + difficulty + "\n";
         // if difficulty is higher than the roll the attack will miss.
         return difficulty > attackRoll;
     }
