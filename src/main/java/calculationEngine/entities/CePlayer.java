356da623fade3674b1ab98dc057e72c76dc5bc20
lkno0705 <lkno0705@gmail.com>
2020-11-06 21:22:20 +0100

    LIT-42 rename Number

--- src/main/java/calculationEngine/entities/CePlayer.java
+++ src/main/java/calculationEngine/entities/CePlayer.java
@@ -6,7 +6,7 @@ public class CePlayer extends CeEntity {
 
     private List<CeEntity> team;
     private boolean isAI = false;
-    private int number;
+    private int playerNumber;
 
     public CePlayer(Nature nature, Attack[] attacks, int hitPoints, int maxHitPoints, int level, int friendshipPoints, int speed, int stamina, int attack, int defense, int developmentLvl, List<CeEntity> team) {
         super(BeastTypes.PlayerStandard, nature, attacks, hitPoints, maxHitPoints, level, friendshipPoints, speed, stamina, attack, defense, developmentLvl, false);
@@ -30,11 +30,11 @@ public class CePlayer extends CeEntity {
     }
 
     public void setNumber(int number){
-        this.number = number;
+        this.playerNumber = number;
     }
 
     public int getNumber(){
-        return number;
+        return playerNumber;
     }
 
 }
