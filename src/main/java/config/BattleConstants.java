9bbeb2f4dfa5b0cccdc257eb6a637ec5659d44c4
BigKahuna7385 <danielburger2@gmx.de>
2021-05-02 16:27:41 +0200

    implemented ne CE and added battleDegub to BattleConstants.

--- src/main/java/config/BattleConstants.java
+++ src/main/java/config/BattleConstants.java
@@ -6,5 +6,6 @@ public class BattleConstants {
 
     public static int tickAmount = 100;
     public static CePlayer noneTurnCePlayer = new CePlayer(null, null, null, false);
+    public static boolean battleDebug = false;
 
 }
