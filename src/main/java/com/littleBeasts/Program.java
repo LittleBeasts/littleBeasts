5152b2d921e3b1e53133e97ce642c07c7ae15ae9
BigKahuna7385 <danielburger2@gmx.de>
2021-05-16 02:50:40 +0200

    - stuff

--- src/main/java/com/littleBeasts/Program.java
+++ src/main/java/com/littleBeasts/Program.java
@@ -29,7 +29,7 @@ public class Program {
         if (args.length > 0)
             startingMap = args[0];
 
-        startingMap = MapNames.residentialArea.toString();
+        startingMap = MapNames.CourtYard_Dorms_Library.toString();
         // initialize the game infrastructure
         Game.init();
 
